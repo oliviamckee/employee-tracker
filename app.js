05dcdf945f4b416603652af8c4d08cd1dd3b9d8e
Olivia McKee <olivia.mckee97@gmail.com>
2022-07-16 19:05:27 -0500

    protect info

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const express = require('express');
 const db = mysql.createConnection({
   host: 'localhost',
   // Your MySQL username,
-  user: 'root',
+  user: process.env.DB_USER,
   // Your MySQL password
-  password: 'Phoebe1960!',
+  password: process.env.DB_PASSWORD,
   database: 'employee_tracker'
 });
 
